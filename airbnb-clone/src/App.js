f9e733690c4ecc6a72e4742fd15864bc9630a0d8
nelsonk97 <nelsonkhiev@gmail.com>
2021-01-28 14:57:15 -0500

    airbnb-clone

---
+++ airbnb-clone/src/App.js
@@ -0,0 +1,21 @@
+import './App.css';
+import Home from "./Home"
+import Header from "./Header"
+
+function App() {
+  return (
+    <div className="app">
+      <Header/>
+      <Home/>
+        { /* Header */ }
+        { /* Banner */ }
+          { /* ... */ }
+        { /* Cards */ }
+        { /* Footer */ }
+      { /* Search Page */ }
+        { /* ... */ }
+    </div>
+  );
+}
+
+export default App;
