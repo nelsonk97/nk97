cd351de0e8b9b1e9f72064710ddf13783cf9f3f8
nelsonk97 <nelsonkhiev@gmail.com>
2021-01-29 18:49:05 -0500

    website

--- nelson-website-react/nelson-website-react/src/Body.js
+++ nelson-website-react/nelson-website-react/src/Body.js
@@ -1,15 +1,19 @@
 import React from 'react'
 import "./Body.css"
-import MediaQuery from 'react-responsive'
+import ReactDOM from 'react-dom';
+import Card from '@material-ui/core/Card';
+import Button from '@material-ui/core/Button';
 
 function Body() {
   return (
     <div className="body">
       <div className="body__container">
         <div className="body__hero">
-          <p><span className="body__catchword">New</span> grad.</p>
-          <p><span className="body__catchword">Forward</span> way of thinking.</p>
-          <p><span className="body__catchword">Ready</span> for hire.</p>
+          <p>junior <span className="body__catchword">full stack</span> developer.</p>
+          <p><span className="body__catchword">fanshawe</span> grad.</p>
+          <p><span className="body__catchword">web dev</span> intern.</p>
+        </div>
+        <div className="body__section">
         </div>
       </div>
     </div>
