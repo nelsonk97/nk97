8495897561bbfbcda8886151205ec4c5c83d6d41
nelsonk97 <nelsonkhiev@gmail.com>
2021-01-29 19:47:01 -0500

    website

--- nelson-website-react/nelson-website-react/src/Body.js
+++ nelson-website-react/nelson-website-react/src/Body.js
@@ -1,8 +1,7 @@
 import React from 'react'
 import "./Body.css"
-import ReactDOM from 'react-dom';
-import Card from '@material-ui/core/Card';
-import Button from '@material-ui/core/Button';
+import LinkedInIcon from '@material-ui/icons/LinkedIn';
+import GitHubIcon from '@material-ui/icons/GitHub';
 
 function Body() {
   return (
@@ -12,8 +11,18 @@ function Body() {
           <p>junior <span className="body__catchword">full stack</span> developer.</p>
           <p><span className="body__catchword">fanshawe</span> grad.</p>
           <p><span className="body__catchword">web dev</span> intern.</p>
+          <div className="body__social">
+            <LinkedInIcon style={{marginTop: "15px", marginRight: "20px", height: "40px", width: "40px", color: "#121212"}}/>
+            <GitHubIcon style={{marginRight: "15px",height: "40px", width: "40px", color: "#121212"}}/>
+          </div>
         </div>
         <div className="body__section">
+          <div className="body__box" id="body__box1"></div>
+          <div className="body__box" id="body__box2"></div>
+          <div className="body__box" id="body__box3"></div>
+          <div className="body__box" id="body__box4"></div>
+          <div className="body__box" id="body__box5"></div>
+          <div className="body__box" id="body__box6"></div>
         </div>
       </div>
     </div>
