b6095fd4d31970ac8153eea3fd70f15114a4427f
nelsonk97 <nelsonkhiev@gmail.com>
2021-01-29 23:32:18 -0500

    website

--- nelson-website-react/nelson-website-react/src/Body.js
+++ nelson-website-react/nelson-website-react/src/Body.js
@@ -6,7 +6,17 @@ import GitHubIcon from '@material-ui/icons/GitHub';
 function Body() {
   return (
     <div className="body">
-      
+      <div className="body__hero">
+        <div className="body__hero-left">
+          <p>introducing nelson.</p>
+          <p>junior full stack developer.</p>
+          <p>fanshawe grad.</p>
+          <p>web dev intern.</p>
+        </div>
+        <div className="body__hero-right"></div>
+      </div>
+      <div className="body__section"></div>
+      <div className="body__section"></div>
     </div>
   )
 }
--- nelson-website-react/nelson-website-react/src/Header.js
+++ nelson-website-react/nelson-website-react/src/Header.js
@@ -1,19 +1,11 @@
 import React from 'react'
 import "./Header.css"
-import MenuIcon from '@material-ui/icons/Menu';
-import CloseIcon from '@material-ui/icons/Close';
 
 function Header() {
   return (
-    <div className="body">
-      <div className="header">
-        <div className="sidebar" id="sidebar">
-          <CloseIcon className={"close-button"} onClick={event => document.getElementById("sidebar").style.display = "none"}/>
-        </div>
-        <div className="header__content">
-          <p>nelson</p>
-          <MenuIcon className={"button"} style={{backgroundColor: "inherit"}} onClick={event => document.getElementById("sidebar").style.display = "block"}/>
-        </div>
+    <div className="header">
+      <div className="header__content">
+        <p>nk</p>
       </div>
     </div>
   )
